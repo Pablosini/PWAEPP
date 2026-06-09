cb26b8d42db0181b40ed466a9a847e32bb4ed9f2
PEPE.STUDIO <pawlowski_98@wp.pl>
2026-06-09 23:41:51 +0200

    Downgrade cache version to v1.0.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'epp-pwa-cache-v1.0.8';
+const CACHE_NAME = 'epp-pwa-cache-v1.0.0';
 
 // Ścieżka bazowa wynika z rejestracji SW, dzięki czemu działa na GitHub Pages i lokalnie.
 const scopePath = new URL(self.registration.scope).pathname;
