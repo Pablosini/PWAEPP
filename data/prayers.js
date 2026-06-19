2aca147b792016c5261cf135e7780f6caff531e3
PEPE.STUDIO <pawlowski_98@wp.pl>
2026-06-19 21:13:44 +0200

    Add files via upload

---
+++ data/prayers.js
@@ -0,0 +1,65 @@
+/*
+ * Dane modlitewnika EPP.
+ * Aby ręcznie dodać modlitwę, dopisz na końcu listy kolejny blok:
+ * { title: "Tytuł", text: `Treść modlitwy` },
+ */
+window.EPP_PRAYERS = [
+  {
+    title: "Modlitwa Pielgrzyma",
+    text: `Panie Jezu,
+Wyruszyłem na pątniczy szlak z wiarą i nadzieją w sercu.
+Spraw, aby ten trud drogi: upał, deszcz, ból fizyczny i zmęczenie,
+stały się dla mnie czasem oczyszczenia i zbliżenia do Ciebie.
+
+Pomóż mi w drugim człowieku dostrzegać Twoją obecność,
+uczyć się cierpliwości, pokory i dzielenia się chlebem oraz uśmiechem.
+Niech wstawiennictwo Matki Bożej Jasnogórskiej,
+do której zmierzam, chroni mnie przed zniechęceniem i niebezpieczeństwami na drodze.
+
+Przemieniaj moje serce, bym po powrocie z pielgrzymki
+był lepszym świadkiem Twojej Ewangelii w codziennym życiu. Amen.`
+  },
+  {
+    title: "Anioł Pański",
+    text: `Anioł Pański zwiastował Pannie Maryi.
+I poczęła z Ducha Świętego.
+Zdrowaś Maryjo...
+
+Oto Ja służebnica Pańska.
+Niech mi się stanie według słowa twego.
+Zdrowaś Maryjo...
+
+A Słowo ciałem się stało.
+I mieszkało między nami.
+Zdrowaś Maryjo...
+
+Módl się za nami, święta Boża Rodzicielko.
+Abyśmy się stali godnymi obietnic Chrystusowych.
+
+Módlmy się: Łaskę Twoją, prosimy Cię, Panie, wlej w serca nasze, abyśmy, którzy za zwiastowaniem anielskim Wcielenie Chrystusa, Syna Twego, poznali, przez mękę Jego i krzyż do chwały zmartwychwstania zostali doprowadzeni. Przez Chrystusa, Pana naszego. Amen.`
+  },
+  {
+    title: "Pod Twoją Obronę",
+    text: `Pod Twoją obronę uciekamy się,
+święta Boża Rodzicielko,
+naszymi prośbami racz nie gardzić w potrzebach naszych,
+ale od wszelakich złych przygód racz nas zawsze wybawiać,
+Panno chwalebna i błogosławiona.
+
+O Pani nasza, Orędowniczko nasza,
+Pośredniczko nasza, Pocieszycielko nasza.
+Z Synem swoim nas pojednaj,
+Synowi supremo nas polecaj,
+swojemu Synowi nas oddawaj.`
+  },
+  {
+    title: "Różaniec - Tajemnice Radosne",
+    text: `1. Zwiastowanie Najświętszej Maryi Pannie
+2. Nawiedzenie św. Elżbiety
+3. Narodzenie Pana Jezusa w Betlejem
+4. Ofiarowanie Pana Jezusa w Świątyni
+5. Odnalezienie Pana Jezusa w Świątyni
+
+Wskazówka pielgrzymkowa: Każda dziesiątka to doskonała okazja do refleksji nad kolejnymi krokami Jezusa i Maryi oraz ofiarowania intencji niesionych w sercu.`
+  }
+];
