2edd30c91b799a17cbcde9bf53fb8cab68033f91
PEPE.STUDIO <pawlowski_98@wp.pl>
2026-06-27 22:01:15 +0200

    Add files via upload

---
+++ data/reflections.js
@@ -0,0 +1,86 @@
+/*
+ * Droga Słowa EPP.
+ * Zmień startDate, aby ustawić dzień odblokowania pierwszego rozważania.
+ * Kolejne dni odblokowują się automatycznie dzień po dniu.
+ */
+window.EPP_REFLECTIONS_CONFIG = {
+  startDate: "2026-07-28"
+};
+
+window.EPP_REFLECTIONS = [
+  {
+    day: 1,
+    title: "Dzień 1",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 1`
+  },
+  {
+    day: 2,
+    title: "Dzień 2",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 2`
+  },
+  {
+    day: 3,
+    title: "Dzień 3",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 3`
+  },
+  {
+    day: 4,
+    title: "Dzień 4",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 4`
+  },
+  {
+    day: 5,
+    title: "Dzień 5",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 5`
+  },
+  {
+    day: 6,
+    title: "Dzień 6",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 6`
+  },
+  {
+    day: 7,
+    title: "Dzień 7",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 7`
+  },
+  {
+    day: 8,
+    title: "Dzień 8",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 8`
+  },
+  {
+    day: 9,
+    title: "Dzień 9",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 9`
+  },
+  {
+    day: 10,
+    title: "Dzień 10",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 10`
+  },
+  {
+    day: 11,
+    title: "Dzień 11",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 11`
+  },
+  {
+    day: 12,
+    title: "Dzień 12",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 12`
+  },
+  {
+    day: 13,
+    title: "Dzień 13",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 13`
+  },
+  {
+    day: 14,
+    title: "Dzień 14",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 14`
+  },
+  {
+    day: 15,
+    title: "Dzień 15",
+    text: `TU WKLEJĘ TREŚĆ ROZWAŻANIA NA DZIEŃ 15`
+  }
+];
